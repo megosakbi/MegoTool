d37a98db3d1b2cbcc1f69c3b0086ca1e9127f706
megosakbi <dominiksaniakk@gmail.com>
2026-03-09 03:33:27 +0100

    Update release.js

--- api/release.js
+++ api/release.js
@@ -1,1 +1,23 @@
+export default async function handler(req, res) {
 
+const webhook = process.env.WEBHOOK_URL
+
+const {name, gameid, desc} = req.body
+
+await fetch(webhook,{
+method:"POST",
+headers:{
+"Content-Type":"application/json"
+},
+body:JSON.stringify({
+content:
+"🚀 **New Game Release**\n\n"+
+"🎮 Game: "+name+"\n"+
+"🆔 ID: "+gameid+"\n"+
+"📄 Description: "+desc
+})
+})
+
+res.status(200).json({status:"ok"})
+
+}
